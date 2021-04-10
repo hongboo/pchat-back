144517d8c19c994d9ae7b0b38337703d775ff46b
洪波 <hongboy@yonyou.com>
2021-04-11 00:23:50 +0800

    添加随机数

--- src/main/java/com/alemonice/pchat/controller/UserController.java
+++ src/main/java/com/alemonice/pchat/controller/UserController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.UUID;
+import java.util.Random;
 
 /**
  * Created by HB on 2021/4/6 10:02.
@@ -18,8 +18,17 @@ public class UserController {
     @PostMapping("/getUserId")
     public Object getUserId(String userId) {
         JSONObject json = new JSONObject();
-        json.put("userId", UUID.randomUUID().toString().replaceAll("-", ""));
+        json.put("userId", generateRandomNumber());
         return json;
+    }
+
+    private String generateRandomNumber() {
+        StringBuilder str = new StringBuilder();
+        Random random = new Random();
+        for (int i = 0; i < 9; i++) {
+            str.append(random.nextInt(10));
+        }
+        return str.toString();
     }
 
     @PostMapping("/getUserMap")
